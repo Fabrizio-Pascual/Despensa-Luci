4ae4f720bca4d757c42f57697fbce8673e984da8
Fabrizio Pascual <fpascual624@gmail.com>
2026-07-26 02:49:09 -0300

    imagen fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,9 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-  // sacá el unoptimized, y si tus imágenes vienen de Supabase/Vercel Blob:
-  remotePatterns: [
-    { protocol: 'https', hostname: 'TU_SUPABASE_PROJECT.supabase.co' },
-    { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' },
-  ],
+  unoptimized: true,
 },
+
   experimental: {
     turbo: {
       enabled: false,
